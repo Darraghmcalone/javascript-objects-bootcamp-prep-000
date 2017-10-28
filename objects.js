3b509ca92f39faeeeac0e5d9d66879b409376383
darraghmcalone@hotmail.co.uk <darraghmcalone@hotmail.co.uk>
2017-10-28 11:53:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@
 var playlist = {
   coldplay:`fix you`,
 };
-function updatePlaylist(playlist, artistName, songTitle
+function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songtitle;
   return playlist;
+}
